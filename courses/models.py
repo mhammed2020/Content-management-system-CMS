77c27e895258a846de793984e1b7810bfbb3c7a5
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-24 21:04:19 +0100

    Edit the models.py file of the courses application

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -32,3 +33,11 @@ class Module(models.Model):
     description = models.TextField(blank=True)
     def __str__(self):
         return self.title
+
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,related_name='contents',on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
