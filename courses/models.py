a645fce33829a196859c07e3dd13d7f31cf9b65e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-24 14:17:44 +0100

    build Module class

--- courses/models.py
+++ courses/models.py
@@ -23,3 +23,12 @@ class Course(models.Model):
         ordering = ['-created']
     def __str__(self):
         return self.title
+
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course,related_name='modules',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.title
