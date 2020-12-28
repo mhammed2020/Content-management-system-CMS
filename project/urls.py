4cab33399cf2611e484ca06f8bc7418677208e7c
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-28 13:21:45 +0100

    add LoginView and LogoutView CBVs

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,8 @@ from django.urls import path
 
 from django.contrib.auth import views as auth_views #Adding an authentication system
 urlpatterns = [
+
+    path('accounts/login/', auth_views.LoginView.as_view(),name='login'),
+path('accounts/logout/', auth_views.LogoutView.as_view(),name='logout'),
     path('admin/', admin.site.urls),
 ]
